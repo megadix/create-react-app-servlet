284dd7abf954b3181721cc81e70738bb4a0308ca
Dimitri De Franciscis <dimitri@megadix.it>
2017-09-20 00:25:04 +0200

    App.js and pom.xml

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter,
+    Link,
+    Route,
+    Switch
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,12 +16,58 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
+          <div>
+            <ul className="nav">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/blog">Blog</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+            <Switch>
+              <Route path="/blog" component={BlogScreen}/>
+              <Route path="/about" component={AboutScreen}/>
+              <Route path="/" component={HomeScreen}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
+}
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+      eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  );
+}
+
+function BlogScreen() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <h4>Some blog post</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <h4>Another blog post</h4>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <h4>Even more blog post</h4>
+      <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  );
+}
+
+function AboutScreen() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+      eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  );
 }
 
 export default App;
